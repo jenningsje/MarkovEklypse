bb6b156c715aaeb94132b55f27cc849835fd72a0
James <jenningsje@mac.com>
2024-10-09 14:07:18 -0500

    imports fetch_get.js now

--- fetch-get/fetch_get.js
+++ fetch-get/fetch_get.js
@@ -1,9 +1,12 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       document.getElementById("demo").innerHTML = xhttp.responseText;
-    }
-};
-xhttp.open("GET", "https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi", true);
-xhttp.send();
+function fetch_get() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+        document.getElementById("demo").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi", true);
+    xhttp.send();
+}
+export { fetch_get };
