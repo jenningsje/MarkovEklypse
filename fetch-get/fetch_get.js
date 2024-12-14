5327f8b3041333854eeb132065c20c5a7ebb978e
James <jenningsje@mac.com>
2024-12-14 13:09:16 -0600

    expand MarkovBot knowledge, prettify Simulator

--- fetch-get/fetch_get.js
+++ fetch-get/fetch_get.js
@@ -1,4 +1,4 @@
-function fetch_get() {
+function fetch_arxiv() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -6,6 +6,17 @@ function fetch_get() {
         document.getElementById("demo").innerHTML = xhttp.responseText;
         }
     };
-    xhttp.open("GET", "https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi", true);
+    xhttp.open("GET", "http://export.arxiv.org/api/query?search_query=all:electron", true);
     xhttp.send();
 }
+
+// URLs of the APIs you want to fetch data from
+const apiUrls = [
+    "https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi", //medical papers
+    "http://export.arxiv.org/api/query?search_query=all:electron", // physics papers
+    "https://api.biorxiv.org",  // biology, biochemistry, medicine, biotech 
+    "https://inspirehep.net/api/literature", // physics papers
+    "https://share.osf.io/api/v2/rawdata/", // Repository and archive for study designs, research materials, data, manuscripts, etc
+    "https://api.codex.jaagrav.in"  // software development
+    
+];
